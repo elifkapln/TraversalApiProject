42bc5fadbfec3ce9d16ca4d17ef25b29f282b0be
Elif Kaplan <elifkplnn61@outlook.com>
2023-09-15 17:34:10 +0300

    Proje dosyası ekle.

---
+++ TraversalApiProject/DAL/Context/VisitorContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TraversalApiProject.DAL.Entities;
+
+namespace TraversalApiProject.DAL.Context
+{
+    public class VisitorContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-DEQQ0OF8\\SQLEXPRESS;initial catalog=TraversalDbApi;integrated security=true;");
+        }
+
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
